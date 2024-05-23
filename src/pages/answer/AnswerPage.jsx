06a34d81640e55b7c10e40453630dfb2495ef9b0
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-05-23 19:10:22 +0500

    dee

--- src/pages/answer/AnswerPage.jsx
+++ src/pages/answer/AnswerPage.jsx
@@ -10,16 +10,25 @@ const AnswerPage = () => {
         <div className="answer-content">
           <div className="answer-left">
             <h1 className="answer-title">Вопрос-Ответ</h1>
-              {Object.entries(faqData).map(([title, questions]) => (
-                <div className="answer-list" key={title}>
-                  <h2 className="answer-list_title">{title}</h2>
-                  {questions.map((question, index) => (
-                    <Link key={index}>{question}</Link>
-                  ))}
-                </div>
-              ))}
-            </div>
+            {Object.entries(faqData).map(([title, questions]) => (
+              <div className="answer-list" key={title}>
+                <h2 className="answer-list_title">{title}</h2>
+                {questions.map((question, index) => (
+                  <div className="answer-list_item" key={index}>
+                    <Link className="answer-list_link" >
+                      {question}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
+          <div className="answer-right">
+                  <div className="answer-right_item" id="register">
+                      <h1 className="">Регистрация</h1>
+                  </div>
+            </div>
+        </div>
       </Container>
     </Box>
   );
