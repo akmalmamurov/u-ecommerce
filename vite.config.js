c3c469f73cd0ed0fa405c43265347284c71f7629
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-05-29 20:27:22 +0500

    vercel setting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       components: "/src/components",
